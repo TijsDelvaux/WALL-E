35588b16bccc546eec0f7aea7ae265de51fd907f
NeleR <smilieke611@hotmail.com>
2015-03-11 13:15:21 +0100

    knop naar map-activity werkt

--- src/com/mycompany/myfirstindoorsapp/MainActivity.java
+++ src/com/mycompany/myfirstindoorsapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mycompany.myfirstindoorsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 /**
  * Sample Android project, powered by indoo.rs :)
@@ -15,5 +17,13 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+    }
+    
+    public void startPlay(View view) {
+    	//startActivity(new Intent(this, PlayActivity.class));
+    }
+    
+    public void startMap(View view) {
+    	startActivity(new Intent(this, MapActivity.class));
     }
 }
