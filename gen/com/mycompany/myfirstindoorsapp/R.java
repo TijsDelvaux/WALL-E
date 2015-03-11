12a1c3c82b4913fcfedf6fe4be5aef6ff8c3b328
NeleR <smilieke611@hotmail.com>
2015-03-11 12:34:58 +0100

    main activity toont twee knoppen, nog niet geïmplementeerd

--- gen/com/mycompany/myfirstindoorsapp/R.java
+++ gen/com/mycompany/myfirstindoorsapp/R.java
@@ -29,15 +29,18 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int scrat=0x7f020002;
+        public static final int ic_scrat=0x7f020002;
+        public static final int scrat=0x7f020003;
     }
     public static final class id {
-        public static final int imageView1=0x7f090001;
-        public static final int menu_settings=0x7f090002;
-        public static final int txtPosition=0x7f090000;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int imageView1=0x7f090000;
+        public static final int menu_settings=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_map=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
@@ -45,7 +48,10 @@ public final class R {
     public static final class string {
         public static final int app_description=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int camera_button=0x7f060006;
+        public static final int content_scrat=0x7f060007;
         public static final int hello_world=0x7f060001;
+        public static final int map_button=0x7f060005;
         public static final int menu_settings=0x7f060003;
         public static final int title_activity_main=0x7f060004;
     }
